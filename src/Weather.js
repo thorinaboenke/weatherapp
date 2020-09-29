fa3ed18cf88c4fd801ed551d143fd23e5ba81937
Thorina Boenke <thorina-boenke@web.de>
2020-09-29 21:04:58 +0200

    modify dependencies

--- src/Weather.js
+++ src/Weather.js
@@ -20,7 +20,7 @@ export default function Weather() {
   // whenever the city is changed (and only then), call the getWeatherfunction to update the weather state
   useEffect(() => {
     getWeatherInfo(city);
-  }, [city, getWeatherInfo]);
+  }, []);
 
   // 4) Function that is passed to the child to handle submit event, will update the parent state to the current childs state
   const selectCity = (newCity) => {
@@ -31,42 +31,6 @@ export default function Weather() {
     getWeatherInfo(city);
   };
 
-  function CityInput(props) {
-    const [value, setValue] = useState('');
-
-    const handleChange = (event) => {
-      setValue(event.target.value);
-    };
-
-    // INPUT FORM WITH SUBMIT BUTTON needs
-    // 1) functions to handle change of the input text i.e. onChange={handleChange}, this function is defined in the child and updates a state in the child component, here called value
-    // 2) this value needs to be also in the input field , i.e. value={value}
-
-    // 3) needs function to hande submit click, i.e. onSubmit={(event) => {props.handleSubmit(value); event.preventDefault();}}
-    // in case of an event (the submit click, the handle Submit function (passed as a prop from the parent component is called on the current state of value) This replaces the state of the parent component with the current state of the child component). event.preventDefault() prevent rerendering
-
-    return (
-      <>
-        <form
-          onSubmit={(event) => {
-            handleSubmit(value);
-            event.preventDefault();
-          }}
-        >
-          <input
-            value={value}
-            id="city"
-            type="text"
-            label="Enter City"
-            onChange={handleChange}
-            placeholder="City"
-          ></input>
-          <br />
-          <button type="submit">Get Weather</button>
-        </form>
-      </>
-    );
-  }
   const kelvinToCelsius = (tempInKelvin) => {
     const tempInCelsius = tempInKelvin - 273.15;
     return Math.round(tempInCelsius);
@@ -74,7 +38,7 @@ export default function Weather() {
   // pass function to handle Submit as prop to the child
   return (
     <div>
-      <CityInput handleSubmit={selectCity} />
+      <CityInput handleSubmit={handleSubmit} />
       <div className="weather-container">
         <div className="weather">
           {weather ? (
